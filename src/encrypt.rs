bd1da9f15ac96539103318f5bb510e2df374ac34
william <willouchterlony@gmail.com>
2025-03-06 22:49:29 +0000

    fixed blunder

--- src/encrypt.rs
+++ src/encrypt.rs
@@ -1,24 +1,11 @@
+pub trait Encrypt {
+    fn encrypt_string(&mut self,key: i8) -> String;
+}
+
+
 /// this function encrypts a string with caesercipher
 /// # args 
 /// takes string slice to the string you would like to encrypt
 /// # returns
 /// This function return a String
-pub fn encrypt_string(string: &str, key: i8) -> String {
-    let mut finalvalue = String::new();
-    for i in string.chars() {
-        if !i.is_alphabetic() {
-            finalvalue.push(i);
-        } else if i.is_ascii_lowercase() {
-            let i = i as i8;
-
-            let i = ((i - 97 + &key) % 26 + 97) as u8 as char;
-            finalvalue.push(i);
-        } else {
-            let i = i as i8;
-
-            let i = ((i - 65 + &key) % 26 + 65) as u8 as char;
-            finalvalue.push(i);
-        }
-    }
-    finalvalue
-}
+``````````}
